d8696478734ae93ce18fe3de6f3370e013cf2b31
aaronsleepy <aaron@kmong.com>
2022-12-05 20:22:31 +0900

    feat: TextView 추가

---
+++ 01-BasicViews/01-BasicViews/TextView.swift
@@ -0,0 +1,20 @@
+//
+//  TextView.swift
+//  01-BasicViews
+//
+//  Created by Aaron on 2022/12/05.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextView()
+    }
+}
