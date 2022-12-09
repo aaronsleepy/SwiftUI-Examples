46319c52eac933ff188c576ea56374e96e09cbf2
aaronsleepy <aaron@kmong.com>
2022-12-09 23:35:56 +0900

    init: init 02-ViewModifieres 프로젝트

---
+++ 02-ViewModifiers/02-ViewModifiers/_2_ViewModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_ViewModifiersApp.swift
+//  02-ViewModifiers
+//
+//  Created by Aaron on 2022/12/09.
+//
+
+import SwiftUI
+
+@main
+struct _2_ViewModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
