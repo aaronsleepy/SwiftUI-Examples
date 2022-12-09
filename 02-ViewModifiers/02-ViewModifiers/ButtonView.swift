bc7247fcba9af4186dd9da45b523b09b1cbda96b
aaronsleepy <aaron@kmong.com>
2022-12-09 23:42:05 +0900

    feat: ButtonView 추가

---
+++ 02-ViewModifiers/02-ViewModifiers/ButtonView.swift
@@ -0,0 +1,25 @@
+//
+//  ButtonView.swift
+//  02-ViewModifiers
+//
+//  Created by Aaron on 2022/12/09.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Button {
+            print("button tapped")
+        } label: {
+            Text("Click Me")
+        }
+
+    }
+}
+
+struct ButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonView()
+    }
+}
