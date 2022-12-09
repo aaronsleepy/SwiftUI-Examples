2f57afd70713ea125b7bdc949dc79276283a8c43
aaronsleepy <aaron@kmong.com>
2022-12-09 23:38:11 +0900

    ui: Text.font 사용하기

--- 02-ViewModifiers/02-ViewModifiers/TextView.swift
+++ 02-ViewModifiers/02-ViewModifiers/TextView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct TextView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(.system(size: 40, weight: .bold, design: .default))
     }
 }
 
